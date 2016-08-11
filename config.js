1e899aad9ae9621df555a48cb50c890ef3b55073
Fareez Ahamed <fareez.ahamed@gmail.com>
2016-08-11 20:31:35 +0530

    Created the config object fully

--- config.js
+++ config.js
@@ -5,9 +5,38 @@ const fs = require('fs');
 
 module.exports = {
 
+	path : process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME']+'/'+'.swiprc',
+
 	load : function() {
-		var path = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
-		this.configObj = JSON.parse(fs.readFileSync(path+'/'+'.swiprc', 'utf8'));
+		try {
+			this.configObj = JSON.parse(fs.readFileSync(this.path, 'utf8'));	
+		} catch(e) {
+			this.configObj = {
+				proxies : {}
+			};
+		}
+		
+	},
+
+	getProxyList : function() {
+		return this.configObj.proxies.keys();
+	},
+
+	setProxy : function(name, object) {
+		this.configObj[name] = object;
+	},
+
+	getProxy : function(name) {
+		return this.configObj[name];
+	},
+
+	save : function() {
+		try {
+			fs.writeFileSync(this.path, JSON.stringify(this.configObj), 'utf8');
+		} catch(e) {
+			console.log('Unable to write to config file');
+			process.exit();
+		}
 	}
 	
 };
