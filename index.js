9dd521d1ab03ee3d6694f2aa4895a85aa9a5824e
Fareez Ahamed <fareez.ahamed@gmail.com>
2016-08-21 20:09:45 +0530

    Fixed all eslint errors :D

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const program = require('commander');
-const prompt  = require('prompt');
-const config  = require('./config');
+const program = require('commander')
+const prompt = require('prompt')
+const config = require('./config')
 
-loadConfig();
+loadConfig()
 
 program
-  .version('0.0.1');
+  .version('0.0.1')
 
 /**
  * List all the proxies available
@@ -13,24 +13,24 @@ program
 program
   .command('ls')
   .description('List proxies')
-  .action(function() {
-  	for (var i = 0; i < config.getProxyList().length; i++) {
-  		console.log(config.getProxyList()[i]);
-  	}
-  });
+  .action(function () {
+    for (var i = 0; i < config.getProxyList().length; i++) {
+      console.log(config.getProxyList()[i])
+    }
+  })
 
 /**
- *	swip add <name> - Add a new proxy by giving a name
+ * swip add <name> - Add a new proxy by giving a name
  */
 program
   .command('add <name>')
   .description('Add a new proxy')
-  .action(function(name, options){
-    prompt.get(['http-proxy','https-proxy'],function(err, result){
-    	config.setProxy(name,result);
-    	config.save();
-    });
-  });
+  .action(function (name, options) {
+    prompt.get(['http-proxy', 'https-proxy'], function (err, result) {
+      config.setProxy(name, result)
+      config.save()
+    })
+  })
 
 /**
  * swip none - Sets the proxies to default
@@ -38,35 +38,33 @@ program
 program
   .command('none')
   .description('Sets the proxies to default')
-  .action(function() {
-    config.deactivateProxy();
-  });
-
+  .action(function () {
+    config.deactivateProxy()
+  })
 
 /**
  * swip <proxy-name> - Switches to that proxy
  */
 program
   .command('*')
-  .action(function(cmd) {
-      if(config.hasProxy(cmd)) {
-        config.activateProxy(cmd); 
-      }
-      else {
-        console.log('No proxy found with that name');
-      }
-  });
+  .action(function (cmd) {
+    if (config.hasProxy(cmd)) {
+      config.activateProxy(cmd)
+    } else {
+      console.log('No proxy found with that name')
+    }
+  })
 
-//Start parsing the argument
-program.parse(process.argv);
+// Start parsing the argument
+program.parse(process.argv)
 
-if (!program.args.length) program.help();
+if (!program.args.length) program.help()
 
-function loadConfig() {
-	try{
-		config.load();	
-	} catch(e) {
-		console.log("Config file not found");
-		process.exit();
-	}
+function loadConfig () {
+  try {
+    config.load()
+  } catch (e) {
+    console.log('Config file not found')
+    process.exit()
+  }
 }
